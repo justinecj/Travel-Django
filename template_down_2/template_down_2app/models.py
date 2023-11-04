953cb74894dff276db274908aeb2e4b8ad230018
justinecj <justinejose008@gmail.com>
2023-11-04 10:25:28 +0530

    second

--- template_down_2/template_down_2app/models.py
+++ template_down_2/template_down_2app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     name = models.CharField(max_length=250)
-    img = models.ImageField(upload_to='pics')
+    img = models.ImageField(upload_to='pic')
     data = models.TextField()
 
     def __str__(self):
